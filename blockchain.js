6a808b44e6ccbd2702358f9afb5b2247ca8bfcb5
eulices-garces <ulifreelancer@gmail.com>
2022-07-27 14:09:16 -0400

    Initial commit

---
+++ blockchain.js
@@ -0,0 +1,32 @@
+ let provider = new ethers.providers.Web3Provider(window.ethereum)
+let signer
+
+async function connectMetamask() {
+    await provider.send("eth_requestAccounts", []);
+    signer = await provider.getSigner();
+    console.log("Account address:", await signer.getAddress());
+
+    const balance = await signer.getBalance()
+    const convertToEth = 1e18;
+    console.log("account's balance in ether:", balance.toString() / convertToEth);
+}
+
+async function claimTokens() {
+    const runTokenContractAddress = "0x7809cf97e1CBEf3314F7ffE64556CCFE44d6E9fe";
+    const runTokenContractAbi = [
+        "function mintTokens(address account, uint256 amount) public",
+    ];
+    const runTokenContract = new ethers.Contract(runTokenContractAddress, runTokenContractAbi, provider);
+    let convertToWei = 1000000000
+    let amountToClaim = window.totalGweiScore * convertToWei
+    await runTokenContract.connect(signer).mintTokens(signer.getAddress(), amountToClaim.toString())
+}
+
+async function claimNft() {
+    const nftContractAddress = "0x54Bc0Ba6f0E46Df4554817485Dd2FcEB0909b795";
+    const mintContractAbi = [
+        "function mint(uint256 amount) public",
+    ];
+    const nftContract = new ethers.Contract(nftContractAddress, mintContractAbi, provider);
+    await nftContract.connect(signer).mint(window.totalNFTScore.toString())
+}
